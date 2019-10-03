6060491bfee451f2758eca2755daf8e1f43e3dc1
David Henderson <dhend1863@gmail.com>
2019-10-03 01:27:01 -0500

    Logic Complete Reset Button Bug Still there

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,8 +3,12 @@ $("#start").on("click", function () {
     game.loadQuestion();
 })
 
-$(document).on(`click`,`.answer-button`,function(e){
+$(document).on(`click`, `.answer-button`, function (e) {
     game.clicked(e);
+})
+
+$(document).on(`click`, `#reset`, function () {
+    game.reset();
 })
 
 var questions = [{
@@ -66,6 +70,7 @@ var game = {
     counter: 30,
     correct: 0,
     incorrect: 0,
+    unanswered: 0,
     countdown: function () {
         game.counter--;
         $(`#counter`).html(game.counter);
@@ -76,61 +81,91 @@ var game = {
     },
     loadQuestion: function () {
         timer = setInterval(game.countdown, 1000);
-        $(`#subwrapper`).html(`<h2>` + questions[game.currentQuestion].
+        $(`#subwrapper`).html("<h2 id='counter'>30<h2>");
+        $(`#subwrapper`).append(`<h2>` + questions[game.currentQuestion].
             question + `</h2>`);
-        for (var i=0;i<questions[game.currentQuestion].answers.length;i++){
+        for (var i = 0; i < questions[game.currentQuestion].answers.length; i++) {
             $(`#subwrapper`).append(`<button class="answer-button" 
-            id="button-`+i+`" data-name="`+questions[game.currentQuestion].answers[i]+`">`+questions[game.currentQuestion].answers[i]+`</button>`);
-        }
-        
-        },
-        nextQuestion: function () {
-            game.counter = 30;
-            $(`#counter`).html(game.counter);
-            game.currentQuestion++;
-            game.loadQuestion();
-        },
-        timeUp: function () {
-        },
-        results: function () {
-        },
-        clicked: function(e) {
-            clearInterval(timer);
-            if($(e.target).data("name")==questions[game.currentQuestion].
-                correctAnswer){
-                game.answeredCorrectly();
-            }   else { 
-                game.answeredIncorrectly();
-
-            }
-        },
-        answeredCorrectly: function () {
-            console.log("TOUCHDOWN!");
-            clearInterval(timer);
-            game.correct++;
-            $(`#subwrapper`).html(`<h2>TOUCHDOWN!</h2>`);
-            if(game.currentQuestion==questions.length-1){
-                setTimeout(game.results,3*1000);
-            }  else{
-                setTimeout(game.nextQuestion,3*1000);
-            }
-
-        },
-        answeredIncorrectly: function () {
-            console.log("WIDE RIGHT NO GOOD!");
-            clearInterval(timer);
-            game.incorrect++;
-            $(`#subwrapper`).html(`<h2>WIDE RIGHT NO GOOD!</h2>`);
-            if(game.currentQuestion==questions.length-1){
-                setTimeout(game.results,3*1000);
-            } else{
-                setTimeout(game.nextQuestion,3*1000);
-            }
-            
-        },
-        reset: function () {
-
+            id="button-`+ i + `" data-name="` + questions[game.currentQuestion].answers[i] + `">` + questions[game.currentQuestion].answers[i] + `</button>`);
         }
 
+    },
+    nextQuestion: function () {
+        game.counter = 30;
+        $(`#counter`).html(game.counter);
+        game.currentQuestion++;
+        game.loadQuestion();
+    },
+    timeUp: function () {
+        clearInterval(timer);
+        game.unanswered++;
+        $(`#subwrapper`).html(`<h2>DELAY OF GAME</h2>`);
+        $(`#subwrapper`).append(`<h3>The Correct Answer Was:` + questions[game.
+            currentQuestion].correctAnswer + `</h3>`)
+        if (game.currentQuestion == questions.length - 1) {
+            setTimeout(game.results, 3 * 1000);
+        } else {
+            setTimeout(game.nextQuestion, 3 * 1000);
+        }
+    },
+    results: function () {
+        clearInterval(timer);
+        $(`#subwrapper`).html("<h2>AT THE END OF REGULATION</h2>");
+        $(`#subwrapper`).append("<h3>Correct: " + game.correct + "</h3>");
+        $(`#subwrapper`).append("<h3>Incorrect: " + game.incorrect + "</h3>");
+        $(`#subwrapper`).append("<h3>Unanswered: " + game.unanswered + "</h3>");
+        $(`#subwrapper`).append("<button id=`reset`>RESET</button>");
+
+
+
+
+
+    },
+    clicked: function (e) {
+        clearInterval(timer);
+        if ($(e.target).data("name") == questions[game.currentQuestion].
+            correctAnswer) {
+            game.answeredCorrectly();
+        } else {
+            game.answeredIncorrectly();
+
+        }
+    },
+    answeredCorrectly: function () {
+        console.log("TOUCHDOWN!");
+        clearInterval(timer);
+        game.correct++;
+        $(`#subwrapper`).html(`<h2>TOUCHDOWN!</h2>`);
+        if (game.currentQuestion == questions.length - 1) {
+            setTimeout(game.results, 3 * 1000);
+        } else {
+            setTimeout(game.nextQuestion, 3 * 1000);
+        }
+
+    },
+    answeredIncorrectly: function () {
+        console.log("WIDE RIGHT NO GOOD!");
+        clearInterval(timer);
+        game.incorrect++;
+        $(`#subwrapper`).html(`<h2>WIDE RIGHT NO GOOD!</h2>`);
+        $(`#subwrapper`).append(`<h3>The Correct Answer Was:` + questions[game.
+            currentQuestion].correctAnswer + `</h3>`)
+        if (game.currentQuestion == questions.length - 1) {
+            setTimeout(game.results, 3 * 1000);
+        } else {
+            setTimeout(game.nextQuestion, 3 * 1000);
+        }
+
+    },
+    reset: function () {
+        game.currentQuestion = 0;
+        game.counter = 0;
+        game.counter = 0;
+        game.incorrect = 0;
+        game.unanswered = 0;
+        game.loadQuestion();
 
     }
+
+
+}
