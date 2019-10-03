1f7ab051cb02734df94c7de53377ceffb4214f7a
David Henderson <dhend1863@gmail.com>
2019-10-02 22:23:59 -0500

    Questions Completed and Variables created

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-$("#start").on("click",function(){
+$("#start").on("click", function () {
     $("#start").remove();
 })
 
@@ -6,81 +6,79 @@ var questions = [{
     question: "What is #43 Darren Sproles' nickname?",
     answers: ["Little Sproles", "Tank", "Road Runner", "Nigerian Nightmare"],
     correctAnswer: "Tank",
-    image:"assets/images/darren.jpg"
+    image: "assets/images/darren.jpg"
 }, {
     question: "In K-State's Victory over Nebraska in 2002 who led K-State with 228 rushing yards?",
     answers: ["Ell Roberson", "Rock Cartwright", "Josh Scobey", "Darren Sproles"],
     correctAnswer: "Ell Roberson",
-    image:"assets/images/ell.jpg"
+    image: "assets/images/ell.jpg"
 }, {
     question: "Who did K-State open up the season against in the 2012 Big XII Title Season",
     answers: ["Kentucky State", "Missouri State", "South Dakota", "Eastern Kentucky"],
     correctAnswer: "Missouri State",
-    image:"assets/images/ms.jpg"
-},  {
+    image: "assets/images/ms.jpg"
+}, {
     question: "What walk on changed positions mid season and helped lead K-State to a bowl game?",
-    answers: ["Jordy Nelson", "Ryan Muller", "Rock Cartwright", "Kody Cook"],
+    answers: ["Jordy Nelson", "Ryan Mueller", "Rock Cartwright", "Kody Cook"],
     correctAnswer: "Kody Cook",
-    image:"assets/images/kody.jpg"
-},  {
+    image: "assets/images/kody.jpg"
+}, {
     question: "Who leads Kansas State in passing yards for a single season?",
     answers: ["Darrell Dickey", "Michael Bishop", "Josh Freeman", "Jake Waters"],
     correctAnswer: "Jake Waters",
-    image:"assets/images/jw.jpg"
-},  {
+    image: "assets/images/jw.jpg"
+}, {
     question: "How many players has K-State put into the NFL all time?",
     answers: ["99", "127", "63", "147"],
     correctAnswer: "147",
-    image:"assets/images/nfl.jpg"
-},  {
+    image: "assets/images/nfl.jpg"
+}, {
     question: "What K-State wide receiver logged an interception in their career?",
     answers: ["Terrence Newman", "Kevin Lockett", "James Terry", "Jordy Nelson"],
     correctAnswer: "Kevin Lockett",
-    image:"assets/images/klock.jpg"
-},  {
+    image: "assets/images/klock.jpg"
+}, {
     question: "Who is the only K-State QB to lead the Cats' to multiple bowl wins?",
     answers: ["Collin Klein", "Jake Waters", "Michael Bishop", "Johnathan Beasley"],
     correctAnswer: "Johnathan Beasley",
-    image:"assets/images/jb.jpg"
-},  {
+    image: "assets/images/jb.jpg"
+}, {
     question: "Who recieved the most Heisman votes for the year they were up for the award?",
     answers: ["Ell Roberson", "Darren Sproles", "Collin Klein", "Michael Bishop"],
     correctAnswer: "Michael Bishop",
-    image:"assets/images/mb.jpg"
-},  {
-    question:
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    image: "assets/images/mb.jpg"
+}, {
+    question: "Who did Bill Snyder beat to receive his first career victory as Head Coach?",
+    answers: ["KU", "Alabama A&M", "Northern Iowa", "North Texas"],
+    correctAnswer: "North Texas",
+    image: "assets/images/unt.jpg"
 
 }];
+
+var game = {
+    questions:questions,
+    currentQuestions:0,
+    counter:30,
+    correct:0,
+    incorrect:0,
+    countdown: function(){
+    },
+    loadQuestion: function(){
+    },
+    nextQuestion: function(){
+    },
+    timeUp: function(){
+    },
+    results: function(){
+    },
+    clicked: function(){
+    },
+    answeredCorrectly: function(){
+    },
+    answeredIncorrectly: function(){
+    },
+    reset: function(){
+    }
+
+
+}
